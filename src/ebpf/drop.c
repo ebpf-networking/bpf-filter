42b128b1a7e0474ed049928f91de3b1832d8e8c7
Sayandeep Sen <sayandes@in.ibm.com>
2022-11-10 13:08:15 +0000

    ip support

--- src/ebpf/drop.c
+++ src/ebpf/drop.c
@@ -1,11 +1,19 @@
-#include "common.h"
-
+#include <linux/if_packet.h>
+#include <linux/ipv6.h>
+#include <linux/icmpv6.h>
 #include <linux/if_ether.h>
 #include <linux/swab.h>
 #include <linux/pkt_cls.h>
 #include <iproute2/bpf_elf.h>
 #include <linux/ip.h>
-#include <iproute2/bpf_api.h>
+#include <linux/bpf.h>
+#include <linux/in.h>
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_endian.h>
+
+#include "common.h"
+#include "parsing_helpers.h"
+
 
 #ifndef __section
 # define __section(NAME)                  \
@@ -27,7 +35,7 @@
 	(*NAME)(__VA_ARGS__) = (void *)BPF_FUNC_##NAME
 #endif
 
-#define bpf_htonl __builtin_bswap32
+//#define bpf_htonl __builtin_bswap32
 #define bpf_memcpy __builtin_memcpy
 
 #define EGRESS_MODE 0
@@ -35,15 +43,32 @@
 #define MAXELEM 2000
 
 
+
 typedef struct cnt_pkt {
 	uint32_t drop;
 	uint32_t pass;
 } pkt_count;
 
+typedef struct iface_desc {
+  __u8 mac[ETH_ALEN];
+  __u32 ip;
+} iface_desc;
+
+#define IP_LEN 4
+
 struct bpf_elf_map iface_map __section("maps") = {
 	.type           = BPF_MAP_TYPE_HASH,
 	.size_key       = sizeof(uint32_t),
 	.size_value     = ETH_ALEN,
+	.pinning        = PIN_GLOBAL_NS,
+	.max_elem       = MAXELEM,
+};
+
+
+struct bpf_elf_map iface_ip_map __section("maps") = {
+	.type           = BPF_MAP_TYPE_HASH,
+	.size_key       = sizeof(uint32_t),
+	.size_value     = sizeof(__be32),
 	.pinning        = PIN_GLOBAL_NS,
 	.max_elem       = MAXELEM,
 };
@@ -67,8 +92,8 @@ struct bpf_elf_map ingress_iface_stat_map  __section("maps") = {
 
 
 /* helper functions called from eBPF programs */
-static int (*bpf_trace_printk)(const char *fmt, int fmt_size, ...) =
-	        (void *) BPF_FUNC_trace_printk;
+//static int (*bpf_trace_printk)(const char *fmt, int fmt_size, ...) =
+//	        (void *) BPF_FUNC_trace_printk;
 
 static __inline int match_mac( struct __sk_buff *skb, uint32_t mode) {
 
@@ -76,22 +101,38 @@ static __inline int match_mac( struct __sk_buff *skb, uint32_t mode) {
 	char src_fmt[] 	 = "MAC_FILTER: source mac: %x %x %x\n";
 	char matched[] 	 = "MAC_FILTER: MAC MATCHED\n";
 	char unmatched[] = "MAC_FILTER: MAC DID NOT MATCH\n";
+	char ip_matched[] 	 = "IP_FILTER: IP MATCHED\n";
+	char ip_unmatched[] = "IP_FILTER: IP DID NOT MATCH\n";
 
+
+	
 	uint32_t *bytes;
 	pkt_count *inf;
 
 	void *data = (void *)(long)skb->data;
 	void *data_end = (void *)(long)skb->data_end;
 	struct ethhdr  *eth  = data;
+	struct iphdr *ip;
+	struct hdr_cursor nh;
+	int nh_type;
+
+
 	uint32_t idx = skb->ifindex;	
 
 	if(data_end < (void*)eth + sizeof(struct ethhdr))
 		return TC_ACT_SHOT;
 
+	nh.pos = data;
+	nh_type = parse_iphdr(&nh, data_end, &ip);
+	if (nh_type == -1)
+	  return TC_ACT_SHOT;
+	
+
+
 	if(mode == EGRESS_MODE){
-	 	inf = map_lookup_elem(&egress_iface_stat_map, &(idx));
+	 	inf = bpf_map_lookup_elem(&egress_iface_stat_map, &(idx));
 	} else{
-	  	inf = map_lookup_elem(&ingress_iface_stat_map, &(idx));
+	  	inf = bpf_map_lookup_elem(&ingress_iface_stat_map, &(idx));
 	}
 	
 	if (!inf){
@@ -99,7 +140,7 @@ static __inline int match_mac( struct __sk_buff *skb, uint32_t mode) {
 		return TC_ACT_OK;
 	}
 
-	bytes = map_lookup_elem(&iface_map, &(idx));
+	bytes = bpf_map_lookup_elem(&iface_map, &(idx));
 	if (bytes) {
 		__u8 iface_mac[ETH_ALEN];
 		__u8 pkt_mac[ETH_ALEN];
@@ -146,8 +187,33 @@ static __inline int match_mac( struct __sk_buff *skb, uint32_t mode) {
 		return TC_ACT_OK;
 	}
 
-	if(idx < MAXELEM){
-	  lock_xadd(&(inf->pass), 1);
+	// IP addresss match
+	bytes = bpf_map_lookup_elem(&iface_ip_map, &(idx));
+	if (bytes) {
+	  __be32 pkt_ip;
+		if (mode == EGRESS_MODE) {
+		  pkt_ip=ip->saddr;
+		} else {
+		  pkt_ip = ip->daddr;
+
+		}
+		__be32 iface_ip;
+		bpf_memcpy(&iface_ip, bytes, sizeof(__be32));
+		if(iface_ip == pkt_ip){
+		  bpf_trace_printk(ip_matched, sizeof(ip_matched));
+		  lock_xadd(&(inf->pass), 1);
+		  return TC_ACT_OK;	
+
+		} else{
+		  lock_xadd(&(inf->drop), 1);
+		  bpf_trace_printk(ip_unmatched, sizeof(ip_unmatched));
+		  return TC_ACT_SHOT;
+		}
+
+		
+	} else {
+		/* Unable to get iface IP. Let the packet through */
+		return TC_ACT_OK;
 	}
 
 	return TC_ACT_OK;

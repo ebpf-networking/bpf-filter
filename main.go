7f1f69d0841e2426f6220ff521b8e08a97a87d57
Dushyant Behl <dushyantbehl@in.ibm.com>
2022-11-10 17:23:42 +0530

    fix print

--- main.go
+++ main.go
@@ -47,7 +47,7 @@ func makeEntry(ifIdx uint32, mac net.HardwareAddr) *entry {
 	var en entry
 	en.ifIdx = ifIdx
 	en.mac = mac
-	fmt.Printf("created an entry with id %d, mac %s\n", ifIdx, string(mac))
+	fmt.Printf("created an entry with id %d, mac %s\n", ifIdx, mac)
 	return &en
 }
 
